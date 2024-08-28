0cbfef4fce88c2b138747f777fcb579118b6dfc0
Giovanni <giovanni.sokenou2105@gmail.com>
2024-08-28 18:55:45 +0200

    Crud Api rest jpa

--- src/main/java/bj/highfiveuniverity/datapulse/controller/ProductController.java
+++ src/main/java/bj/highfiveuniverity/datapulse/controller/ProductController.java
@@ -2,6 +2,7 @@ package bj.highfiveuniverity.datapulse.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,29 +12,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import bj.highfiveuniverity.datapulse.model.Product;
 import bj.highfiveuniverity.datapulse.repository.ProductRepository;
+
 @RestController
 @RequestMapping("/api/products")
-                              
+
 public class ProductController {
-            private   ProductRepository productRepository;
+               @Autowired
+               private ProductRepository productRepository;
 
                @GetMapping
                public List<Product> getAllProducts() {
-
+                              return productRepository.findAll();
                }
 
                @PostMapping
                public Product createProduct(Product product) {
-
+                              return productRepository.save(product);
                }
 
                @PutMapping("/{id}")
                public Product updateteProduct(Long id, Product productDetail) {
+                              Product product = productRepository.findById(id).orElseThrow();
+                              product.setName(productDetail.getName());
+                              product.setPrice(productDetail.getPrice());
 
+                              return productRepository.save(product);
                }
+
                @DeleteMapping("/{id}")
                public void deleteProduct(Long id) {
-
+                              Product product = productRepository.findById(id).orElseThrow();
+                              productRepository.delete(product);
                }
 
 }
